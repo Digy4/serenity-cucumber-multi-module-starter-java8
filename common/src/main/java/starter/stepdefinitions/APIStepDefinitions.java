39d03ebf47c8caefeaae3045ec5a2c809cffad2a
digy4tech <tech@digy4.com>
2023-09-03 19:50:51 +0200

    add new automated test to test hybrid screenshots customer attribute step duration

--- common/src/main/java/starter/stepdefinitions/APIStepDefinitions.java
+++ common/src/main/java/starter/stepdefinitions/APIStepDefinitions.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.rest.SerenityRest;
 import org.hamcrest.Matchers;
+import net.serenitybdd.core.Serenity;
 
 public class APIStepDefinitions {
 
@@ -17,17 +18,21 @@ public class APIStepDefinitions {
 
     @When("PUT is used")
     public void put_is_used() {
+        Serenity.recordReportData().withTitle("Put Request Custom Data- Name").andContents("{\"name\": \"Jo Bloggs\"}");
         final String requestJson = "{\"name\": \"Jo Bloggs\", \"Job\": \"Quality Engineer\"}";
         SerenityRest.rest().body(requestJson).post(apiUrl);
     }
 
     @When("GET is used")
     public void get_is_used() {
+        Serenity.recordReportData().withTitle("Put Request Custom Data- Name").andContents("{\"name\": \"Jo Bloggs\"}");
         SerenityRest.rest().get(apiUrl);
     }
 
     @Then("the status code is {int}")
     public void the_status_code_is(Integer statusCode) {
+        Serenity.recordReportData().withTitle("Put Request Custom Data- Name").andContents("{\"name\": \"Jo Bloggs\"}");
         SerenityRest.then().statusCode(Matchers.equalTo(statusCode));
+
     }
 }
